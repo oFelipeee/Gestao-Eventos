5d91adb8ec9a262a0fdc0f6afcbf6e7fea367a84
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2024-09-26 14:09:22 -0300

    .

--- server.js
+++ server.js
@@ -10,24 +10,29 @@ app.use(express.json());
 app.use('/evento', routerEvento);
 app.use('/participantes', routerParticipantes);
 
-app.get('/healthcheck', (req,res) => {
-    return res.status(200).json({
-        msg: 'Estamos Vivos!',
-        alive: true
-    })
+app.get('/healthcheck', (req, res) => {
+  return res.status(200).json({
+    msg: 'Estamos Vivos!',
+    alive: true
+  })
 });
 
 sequelize.authenticate()
-.then(() => {
+  .then(() => {
     console.log('Conexão estabelecida com sucesso!');
     return sequelize.sync();
-})
-.then(() => {
+  })
+  .then(() => {
     app.listen(process.env.PORT == null ? 3000 : process.env.PORT, (req, res) => {
-        console.log('Servidor rodando na porta: 3000. http://127.0.0.1:3000')
+      console.log('===================================');
+      console.log('Servidor rodando na porta: 3000');
+      console.log('==================================');
     });
-})
+  })
 
-.catch((error) => {
+  .catch((error) => {
+    console.log('==================================================');
     console.error('Erro ao se conectar com o banco: ', error)
-})
+    console.log('==================================================');
+
+  })
